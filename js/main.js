38e8d3fd7649ea78c3dee58fb14d9f7b281bd09f
Igor <33373781+IgorShutko@users.noreply.github.com>
2018-01-07 03:01:14 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.carousel.carousel-slider').carousel(),
+    $('.slider').slider(),
+    $('input#input_text, textarea#textarea2').characterCounter(),
+    $('.modal').modal(),
+    $('.parallax').parallax();
+});
+
+
